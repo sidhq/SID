fffcf4d3fe58efbf4fb7ea44fb37c213b1ad850e
lotte <lotte@lottes-MacBook-Air.local>
2023-08-09 10:15:49 -0700

    changed texts and a couple other tiny things

--- examples/sid-copywriting/src/components/copyWriting.tsx
+++ examples/sid-copywriting/src/components/copyWriting.tsx
@@ -3,6 +3,7 @@ import styles from "@/styles/CopyWriting.module.scss";
 import {json} from "stream/consumers";
 
 interface Template {
+    type: string;
     buttonText: string;
     backgroundImage: string;
     input: string;
@@ -49,35 +50,47 @@ export default function CopyWriting() {
     );
 
     const templates: Template[] = [{
-        buttonText: 'Twitter Thread',
-        backgroundImage: 'url(/static/images/bird.svg)',
-        input: 'Write a twitter thread about SID...',
-        outputWithSID: 'SID is an AI that crafts tweets based on millions of existing tweets.',
-        outputWithoutSID: 'Craft tweets with the precision of human-like quality.',
+        buttonText: 'Twitter Post',
+        type: 'text',
+        backgroundImage: 'url(/static/images/twitter.svg)',
+        input: 'Write a Twitter post explaining what SID does',
+        outputWithSID: 'Struggling with making your AI more context-aware? SID simplifies the integration of customer data. Our API seamlessly handles all data integrations, storage, and retrieval. We save you months of engineering effort in a single afternoon.',
+        outputWithoutSID: 'SID (System Identification) is a powerful technique used in engineering to model and analyze dynamic systems. It helps us understand and predict the behavior of complex systems, enabling us to design efficient control strategies.',
     }, {
         buttonText: 'Sales Outreach',
-        backgroundImage: 'url(/static/images/handshake.svg)',
-        input: 'Write a sales outreach email to a potential customer...',
-        outputWithSID: 'SID crafts persuasive sales emails by analyzing millions of outreach messages.',
-        outputWithoutSID: 'Generate compelling sales emails effortlessly.',
-    }, {
-        buttonText: 'Customer Support Email',
+        type: 'mail',
         backgroundImage: 'url(/static/images/mail.svg)',
-        input: 'Write a customer support email to a customer...',
-        outputWithSID: 'SID can write empathetic customer support emails based on vast data.',
-        outputWithoutSID: 'Achieve excellent customer support with finely-tuned emails.',
+        input: 'Write 3 sentences explaining why the copywriting AI company <company name> could use SID. Use an example of how linking mail and knowledge bases can help their product.',
+        outputWithSID: 'SID\'s API simplifies the integration of user data into AI applications. For example, <Company Name> could get detailed context on each user\'s prompt by enabling them to link their knowledge base and email history. This allows <Company Name> generate more tailored, precise and effective copy.',
+        outputWithoutSID: 'Using Sentence Intent Detection (SID), <company mame>, can generate more relevant and targeted content by understanding the intent behind each sentence. For example, when creating emails, SID can discern whether the user wants to inform, persuade, or inquire. Whether creating knowledge bases or generating emails, SID ensures that <cmpany name> content is accurate and aligned with the user\s intent.',
     }, {
-        buttonText: 'Launch Presentation',
+        buttonText: 'Random Quote',
+        type: 'chat',
+        backgroundImage: 'url(/static/images/brain.svg)',
+        input: 'What did <name> say about benchmarking?',
+        outputWithSID: '<name> said that benchmarks are sometimes overrated and that testing on real user feedback can be more reliable. <name> mentioned that relying too much on benchmarks can lead to tuning solely for those benchmarks rather than focusing on the actual performance and quality of the product.',
+        outputWithoutSID: 'I\'m sorry, but as an AI, I don\'t have access to specific databases or personal data unless it has been shared with me in the course of our conversation. Therefore, I don\'t have any specific information about what <name> said about benchmarking. However, I can provide general information about benchmarking.',
+    },{
+        buttonText: 'Personal Mail',
+        type: 'mail',
+        backgroundImage: 'url(/static/images/handshake.svg)',
+        input: 'Write a casual first sentence for an email to <name> that highlights our uncommon commonalities',
+        outputWithSID: 'I hope this email finds you well amidst the <city> startup hustle! As fellow <university> alumni, it\'s amusing to discover that w\'ve both mastered the art of balancing business acumen with coding finesse – who would\'ve thought? 😄',
+        outputWithoutSID: 'Isn\'t it wild how we both seem to march to the beat of the same offbeat drum, sharing some pretty uncommon commonalities?',
+    }, {
+        buttonText: 'Feature Presentation',
+        type: 'text',
         backgroundImage: 'url(/static/images/stocks.svg)',
-        input: 'Write a presentation for the launch of SID...',
-        outputWithSID: 'SID crafts presentations based on millions of slides, ensuring quality.',
-        outputWithoutSID: 'Design powerful launch presentations with ease.',
+        input: 'What features would make SID\'s product better?',
+        outputWithSID: 'Feature 1: Expanded Data Source Integration: Adding support for more data sources like Dropbox, Slack, or CRM platforms would allow SID to access a wider range of user data, enabling more comprehensive and personalized AI services. Feature 2. Advanced NLP Capabilities: Enhancing SID\'s NLP capabilities could involve improving sentiment analysis and entity recognition. This would enable a more accurate and nuanced understanding of user data and generate more context-aware responses.',
+        outputWithoutSID: 'I\'m sorry, but I don\'t have information about a specific project or product called "SID" in my training data up until September 2021. Could you please provide more context or details about what "SID" refers to? That way, I can try to offer suggestions on how to improve it.',
     }, {
-        buttonText: 'Other Examples',
-        backgroundImage: 'url(/static/images/rocket-ship.svg)',
-        input: 'Write a...',
-        outputWithSID: 'SID is versatile, trained on millions of documents for accurate writing.',
-        outputWithoutSID: 'Harness AI\'s potential for diverse writing tasks.',
+        buttonText: 'Travel Plans',
+        type: 'chat',
+        backgroundImage: 'url(/static/images/mountain.svg)',
+        input: 'Give me an overview of what we\'ve decided on for Iceland',
+        outputWithSID: 'The trip will take place from <date> to <date>. <name> will arrive at <time> and will have some time to entertain themselves in <city> until you and <name> arrive around <time>. The first two days will be spent driving along the south coast, while the remaining three days will be spent in <city>. <name> will contact a guide to inquire about the possibility of hiking to an active volcano on <date>.',
+        outputWithoutSID: 'I\'m sorry, but I don\'t have access to previous conversations or decisions made regarding Iceland. As an AI language model, I don\'t have the capability to retain information from previous interactions. If you have any specific questions, feel free to ask, and I\'ll do my best to help you.'
     }];
     const typeInTerminal = async (delay: number, perMessage: boolean, target: string): Promise<void> => {
         return new Promise(async (resolve) => {
@@ -236,9 +249,9 @@ export default function CopyWriting() {
         <div className={styles.mainWrapperCopyWriting}>
             <div className={styles.taskContainer}>
                 <div className={styles.taskHeader}>
-                    <span className={styles.taskText}>Task:</span>
+                    <div className={styles.taskText}>Task:</div>
                     <input className={styles.taskTextBox}
-                           placeholder={'Write a twitter thread about SID...'}
+                           placeholder={'Select an example to see SID in action!'}
                            ref={typingState ? typingState.get('inputRef')?.typingRef as unknown as React.RefObject<HTMLInputElement> : null}
                            disabled={true}
                            value={typingState ? typingState.get('inputRef')?.typingOutput.map((message) => {
