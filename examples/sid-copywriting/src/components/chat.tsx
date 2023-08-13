26d9f953928f85c99f1d62e7634cab4fa18383e6
Lukas Ruflair <lukas@ruflair.com>
2023-08-12 20:30:48 -0700

    feat: Improve chat functionality and style alignment #51ee6d12
    Refactored 'CopyWriting' function to 'Chat' in chat.tsx for better clarity.
    Replaced 'inputRef', 'withSIDRef', and 'withoutSIDRef' with 'secondChatMessage'
    and 'thirdChatMessage' to better represent their functionality.
    Added useEffect hooks to manage typingState and template changes.
    Replaced hardcoded chat messages with dynamic content from templates.
    Disabled input field as it is not being used presently.
    In Chat.module.scss, added 'align-self: flex-end' to chat messages for better alignment.

--- examples/sid-copywriting/src/components/chat.tsx
+++ examples/sid-copywriting/src/components/chat.tsx
@@ -3,24 +3,17 @@ import styles from "@/styles/Chat.module.scss";
 import {TypingState, DemoProps} from "@/types";
 import {typeInTerminal} from "@/utils";
 
-export default function CopyWriting({template}: DemoProps) {
+export default function Chat({template}: DemoProps) {
     const [typingState, setTypingState] = useState<TypingState>(
         new Map([
-            ['inputRef', {
-                typingRef: React.createRef<HTMLInputElement>(),
-                typingQueue: [],
-                typingOutput: [],
-                isTyping: false,
-                typingInterval: null,
-            }],
-            ['withSIDRef', {
+            ['secondChatMessage', {
                 typingRef: React.createRef<HTMLParagraphElement>(),
                 typingQueue: [],
                 typingOutput: [],
                 isTyping: false,
                 typingInterval: null,
             }],
-            ['withoutSIDRef', {
+            ['thirdChatMessage', {
                 typingRef: React.createRef<HTMLParagraphElement>(),
                 typingQueue: [],
                 typingOutput: [],
@@ -30,26 +23,82 @@ export default function CopyWriting({template}: DemoProps) {
         ])
     );
 
+
+    useEffect(() => {
+        if (typingState && !typingState.get('secondChatMessage')?.isTyping && !typingState.get('thirdChatMessage')?.isTyping) {
+            typeInTerminal(16, false, 'secondChatMessage', typingState, setTypingState).catch((err) => {
+                console.log(err);
+            });
+            typeInTerminal(20, false, 'thirdChatMessage', typingState, setTypingState).catch((err) => {
+                console.log(err);
+            });
+        }
+        return () => {
+        };
+    }, [typingState]);
+
+    useEffect(() => {
+        //This triggers when a user selects a template
+        if (template) {
+            setTypingState(prev => {
+                const newTypingState = new Map(prev);
+                for (const [key, value] of Object.entries({
+                    'secondChatMessage': template.outputWithSID,
+                    'thirdChatMessage': template.outputWithoutSID,
+                })) {
+                    const refObj = newTypingState.get(key);
+                    if (refObj) {
+                        refObj.typingQueue.length = 0;
+                        refObj.typingQueue.push(value);
+                        newTypingState.set(key, refObj);
+                    }
+                }
+
+                return newTypingState;
+            });
+        }
+        return () => {
+            //cleanup
+            typingState?.forEach((value, key) => {
+                clearInterval(value.typingInterval || undefined);
+                setTypingState(prev => {
+                    const newTypingState = new Map(prev);
+                    const targetTypingState = newTypingState.get(key);
+                    if (targetTypingState) {
+                        targetTypingState.isTyping = false;
+                        targetTypingState.typingOutput = [];
+                    }
+                    return newTypingState;
+                });
+            });
+        };
+    }, [template]);
+
+
     return (
         <div className={styles.mainWrapperChat}>
             <div className={styles.chatWrapper}>
                 <div className={styles.chatBody}>
                     <div className={`${styles.chatMessage} ${styles.humanMessage}`}>
-                        <p>What does SID do?</p>
+                        <p>{template?.input}</p>
                     </div>
                     <div className={`${styles.chatMessage} ${styles.sidgptMessage}`}>
                         <img src="static/images/sid-emoji.svg" alt="SID"/>
-                        <p>SID is an AI assistant that can run in the background of your device. It can be called upon at any time to answer questions you have. SID's capabilities can be extended by linking directories and...</p>
+                        {typingState && typingState.get('secondChatMessage')?.typingOutput[0] ?
+                            <p>{typingState.get('secondChatMessage')?.typingOutput[0]}</p>
+                            : ''}
                     </div>
                     <div className={`${styles.chatMessage} ${styles.chatgptMessage}`}>
-                        <img src="static/images/chatgpt-emoji.svg" alt="ChatGPT"/>
-                        <p>Could you please provide more context? SID could refer to a number of things, such as a Security Identifier in computing or Sudden Infant Death syndrome in health</p>
+                        <img src="static/images/chatgpt-emoji.svg" alt="SID"/>
+                        {typingState && typingState.get('secondChatMessage')?.typingOutput[0] ?
+                            <p>{typingState.get('thirdChatMessage')?.typingOutput[0]}</p>
+                            : ''}
                     </div>
                 </div>
                 <div className={styles.chatInput}>
                     <div className={styles.chatInputField}>
                         <input type="text"
-                               ref={typingState ? typingState.get('inputRef')?.typingRef as unknown as React.RefObject<HTMLInputElement> : null}
+                               disabled={true}
                                placeholder={'Type your message here...'}/>
                         <button>
                             <img src="static/images/paperplane.svg" alt="Send"/>
