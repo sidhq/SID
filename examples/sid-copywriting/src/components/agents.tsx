d2fd01f10f19608687fc334f3220a4eb64e3ed3b
Lukas Ruflair <lukas@ruflair.com>
2023-08-22 23:06:57 -0700

    :art: Update href target in agents.tsx and modify highlight color in _variables.scss

--- examples/sid-copywriting/src/components/agents.tsx
+++ examples/sid-copywriting/src/components/agents.tsx
@@ -7,7 +7,7 @@ export default function Agents({template}: DemoProps) {
         <div className={styles.mainWrapperAgents}>
             <div>
                 <p>Check out
-                    <a className={styles.agentGPTLogo} href={'https://agentgpt.reworkd.ai/'}>
+                    <a className={styles.agentGPTLogo} href={'https://agentgpt.reworkd.ai/'} target={"_parent"}>
                         <img src="/static/images/reworkd-logo.svg" alt="Agents" className={styles.agentsImage}/>
                         <span className={styles.agentSpan}>Agent</span><span className={styles.gptSpan}>GPT</span>
                     </a>to see SID in action!</p></div>
